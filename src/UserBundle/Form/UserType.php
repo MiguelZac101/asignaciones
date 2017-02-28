349b75f607cc63fdbe7ec29a8ca7f6ba84941ef9
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-02-28 10:53:31 -0500

    nuevo

---
+++ src/UserBundle/Form/UserType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email','email')
+            ->add('password','password')
+            ->add('role','choice',array('choices'=>array('ROLE_ADMIN'=>'ADMINISTRATOR','ROLE_USER'=>'USER')))
+            ->add('isActive','checkbox')
+            ->add('save','submit',array('label'=>'save user'))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'UserBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'user';
+    }
+}
