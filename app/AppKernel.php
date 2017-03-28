6d4f91ca0771cb94ddab7a8aad4cdc8db6c78565
Miguel Quispe <miguel.quispe@fractal.com.pe>
2017-03-28 15:39:43 -0500

    pendiente

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new UserBundle\UserBundle(),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle()         
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
